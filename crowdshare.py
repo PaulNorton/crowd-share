7d58ac894023a4fa05b371877b8575b961f238ff
Paul Norton <paul@nortoncrew.com>
2017-02-04 23:46:31 -0500

    Add caption

--- crowdshare.py
+++ crowdshare.py
@@ -51,8 +51,9 @@ class App(Frame):
             self.bucket_name = self.build_bucket_name(self.HASHTAG, datetime.now())            
             self.bucket = self.s3.create_bucket(Bucket=self.bucket_name)
 
-        self.x = 1
+        self.x = 0
         self.pics = []
+        self.text = ''
 
         self.id = self.after(3000, self.callback)
 
@@ -61,7 +62,7 @@ class App(Frame):
 
     def resize(self, width, height):
         width -= 40
-        height -= 40
+        height -= 120
         if height/width > self.original.height/self.original.width:
             size = (width, int(width*self.original.height/self.original.width))
         else:
@@ -69,7 +70,9 @@ class App(Frame):
         resized = self.original.resize(size,Image.ANTIALIAS)
         self.image = ImageTk.PhotoImage(resized)
         self.display.delete('IMG')
-        self.display.create_image(width/2+20, height/2+20, image=self.image, anchor=CENTER, tags='IMG')        
+        self.display.delete('TEXT')
+        self.display.create_image(width/2+20, height/2+20, image=self.image, anchor=CENTER, tags='IMG')
+        self.display.create_text(width/2+20, height+100, anchor=S, text=self.text, tags='TEXT', fill='white', font=("Purisa", 18), width=width, justify=CENTER)
 
     def callback(self):
         self.search_twitter()
@@ -80,15 +83,21 @@ class App(Frame):
         
     def set_image(self):
         if len(self.pics) > 0:
-            if self.x > len(self.pics):
-                self.x = 1
+            if self.x == len(self.pics):
+                self.x = 0
+                
+            post = self.pics[self.x]
+            
             if self.AWS:
-                bytes = self.retrieve_from_aws(str(self.x) + '.jpg')
+                bytes = self.retrieve_from_aws(post.file_name)
                 self.original = Image.open(io.BytesIO(bytes))
             else:
-                self.original = Image.open(str(self.x) + '.jpg')
+                self.original = Image.open(post.file_name)
+            
             self.image = ImageTk.PhotoImage(self.original)
+            self.text = post.message
             self.resize(self.display.winfo_width(), self.display.winfo_height())
+            
             self.x += 1
 
     def search_twitter(self):
@@ -101,9 +110,10 @@ class App(Frame):
             for item in media:
                 if item['type'] == 'photo':
                     url = item['media_url_https']
-                    if url not in self.pics:
-                        self.pics.append(url)
-                        self.save_to_aws(url)
+                    if not any(x.id == item['id'] for x in self.pics):
+                        post = Post(id=item['id'], user_name=status['user']['screen_name'], platform='twitter', file_name=str(len(self.pics))+'.jpg', url=url, text=status['text'])
+                        self.save_to_aws(post)
+                        self.pics.append(post)
     
     def search_instagram(self):
         r = requests.get('https://api.instagram.com/v1/tags/' + self.HASHTAG + '/media/recent?access_token=' + self.ACCESS_TOKEN)
@@ -111,16 +121,17 @@ class App(Frame):
         posts = json_data['data']
         for post in posts:
             url = post['images']['standard_resolution']['url']
-            if url not in self.pics:
-                self.pics.append(url)
-                self.save_to_aws(url)
+            if not any(x.id == post['id'] for x in self.pics):
+                post = Post(id=post['id'], user_name=post['user']['username'], platform='instagram', file_name=str(len(self.pics))+'.jpg', url=url, text=post['caption']['text'])
+                self.save_to_aws(post)
+                self.pics.append(post)
                 
-    def save_to_aws(self, url):
+    def save_to_aws(self, post):
         if self.AWS:
-            r = requests.get(url)
-            self.bucket.put_object(Key=str(len(self.pics)) + '.jpg', Body=r.content)
+            r = requests.get(post.url)
+            self.bucket.put_object(Key=post.file_name, Body=r.content)
         else:
-            urlretrieve(url, str(len(self.pics)) + '.jpg')
+            urlretrieve(post.url, post.file_name)
             
     def retrieve_from_aws(self, key):
         object = self.s3.Object(self.bucket_name,key)
@@ -129,6 +140,19 @@ class App(Frame):
     def build_bucket_name(self, hashtag, date):
         date_str = str(date).replace(' ', '-').replace(':', '-').split('.')[0]
         return hashtag + '-' + date_str
+
+class Post:
+    def __init__(self, id, user_name, platform, file_name, url, text):
+        self.id = id
+        self.user_name = user_name
+        self.platform = platform
+        self.file_name = file_name
+        self.url = url
+        
+        text = text.encode('ascii', 'ignore').decode().strip()
+        self.text = (text[:75] + '...') if len(text) > 75 else text
+        
+        self.message = '"' + self.text + '" - ' + self.user_name + ' via ' + self.platform
 
 root = MyTk()
 app = App(root)
